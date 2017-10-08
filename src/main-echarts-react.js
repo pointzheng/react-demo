ec023f8e1347f32d4d8bf99674a3aa09cef74f1d
zhengyy <zhengyy@calis.edu.cn>
2017-10-08 18:37:01 +0800

    添加echarts-for-react的例子

---
+++ src/main-echarts-react.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ReactEcharts from 'echarts-for-react'; 
+import ReactDOM from 'react-dom';
+
+
+
+let options = {title: {
+    text: 'ECharts 入门示例'
+  },
+  tooltip: {},
+  xAxis: {
+    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+  },
+  yAxis: {},
+  series: [{
+    name: '销量',
+    type: 'bar',
+    data: [5, 20, 36, 10, 10, 20]
+  }]}
+ReactDOM.render(
+		<ReactEcharts
+		  option={options}
+		  notMerge={true}
+		  lazyUpdate={true}
+		  theme={"theme_name"}
+		 
+		   />,
+	
+    document.getElementById('root')
+);
