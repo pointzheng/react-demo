3bd396c5ab7f5a823df72ba649101d2636de15d5
zhengyy <zhengyy@calis.edu.cn>
2017-10-08 11:25:44 +0800

    加入echarts示例

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Hello from './greeter.js';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+// 尝试导入jquery
 class Demo1 extends Component {
     render() {
     	
@@ -12,7 +13,29 @@ class Demo1 extends Component {
     }
 }
 
-ReactDOM.render(
-    <Demo1 value={"test"} />,
-    document.getElementById('root')
-);
+
+// 尝试导入echarts
+var echarts = require('echarts');
+//基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('root'));
+//绘制图表
+myChart.setOption({
+ title: { text: 'ECharts 入门示例' },
+ tooltip: {},
+ xAxis: {
+     data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+ },
+ yAxis: {},
+ series: [{
+     name: '销量',
+     type: 'bar',
+     data: [5, 20, 36, 10, 10, 20]
+ }]
+});
+
+//ReactDOM.render(
+////  <Demo1 value={"test"} />,
+////	<Hello />,
+//	
+//    document.getElementById('root')
+//);
