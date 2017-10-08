25788a4b5e4b64fcdbfaa6342e31c3eddf6efb18
zhengyy <zhengyy@calis.edu.cn>
2017-10-08 10:38:01 +0800

    加入jquery

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Hello from './greeter.js';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+class Demo1 extends Component {
+    render() {
+    	
+    	console.log("jquery is:" + window.jQuery + ",length:" + $(".test").length);
+        return (
+            <input defaultValue={this.props.value} />
+        )
+    }
+}
+
 ReactDOM.render(
-    <Hello />,
+    <Demo1 value={"test"} />,
     document.getElementById('root')
 );
